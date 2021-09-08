f79e9aa85dfa50169b8ac4f3c1502234d504038f
sllyons22 <90272613+sllyons22@users.noreply.github.com>
2021-09-08 10:13:35 -0400

    Initial commit

---
+++ EmployeesApi/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class DemoController : ControllerBase
+    {
+        // In the Url itself (RouteData)
+        // GET /products/83989389
+        [HttpGet("products/{sku:int}")]
+        public ActionResult LookUpProduct(int sku)
+        {
+            return Ok($"Here is product {sku}");
+        }
+
+        [HttpGet("/orders/{year:int}/{month:int:range(1,12)}/{day:int:range(1,31)}")]
+        public ActionResult GetOrderForDate(int year, int month, int day)
+        {
+            return Ok($"Getting orders for {year}-{month}-{day}");
+        }
+    }
+}
+
---
+++ EmployeesApi/Controllers/StatusController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        // GET /status -> 200
+        [HttpGet("status")] // Attributes
+        public ActionResult GetTheStatus()
+        {
+
+            var response = new GetStatusResponse
+            {
+                Message = "The server is running great!",
+                WhenChecked = DateTime.Now
+            };
+            return Ok(response);
+        }
+    }
+
+
+    public class GetStatusResponse
+    {
+        public string Message { get; set; }
+        public DateTime WhenChecked { get; set; }
+    }
+
+}
